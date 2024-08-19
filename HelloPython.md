dc174024e7dc19b9dfd65cdacdccacd29057c4d4
shgopher <shgopher@gmail.com>
2024-08-19 19:14:57 +0800

    feat: 🎸 add types

--- HelloPython.md
+++ HelloPython.md
@@ -2,7 +2,7 @@
  * @Author: shgopher shgopher@gmail.com
  * @Date: 2024-08-18 11:41:33
  * @LastEditors: shgopher shgopher@gmail.com
- * @LastEditTime: 2024-08-19 17:16:33
+ * @LastEditTime: 2024-08-19 19:02:40
  * @FilePath: /PythonFamily/HelloPython.md
  * @Description: 
  * 
@@ -19,6 +19,50 @@ Python 中的变量没有类型，它可以被赋予任何类型的参数
 python 没有字符，字符串是一个的就是字符，通常我们喜欢使用 ‘’ 表示字符
 
 - 字符串
-- number：int float bool (bool 是 number 的子类 0 1 表示 false true) complex
-- list 就是不同类型的切片 [1,1.0,true，“1.0”]，跟元组也不一样，元组虽然是不同类型的，但是元组的元素不能修改，list 可以修改，所以你可以理解为它是不同类型的切片，或者一个只拥有 key 的 map
 
+- number：int float bool (bool 是 number 的子类 0 1 表示 false true) complex
+
+- list 就是不同类型的切片 [1,1.0,true，“1.0”]，跟元组也不一样，元组虽然是不同类型的，但是元组的元素不能修改，list 可以修改，所以你可以理解为它是不同类型的切片
+
+- tuple 元组，可以理解为不可修改的 list a =(1,1.0，‘1.0’)
+
+- Set 可以理解为只有 key 的 map a = {1,1.0，‘1.0’}
+
+- 字典，或者也可以叫 map 就是一般意义的那个 key-value 的 map a = {1:1.0,1.0：‘1.0’} key 具有唯一性，set 也一样
+
+- bytes 类型不可变的二进制序列 a = b'hello' 或者 a= bytes (‘hello’，‘utf-8’) 其实就是字符类型，默认使用 utf-8 编码通过内置函数 ord 将字符类型转化为编码中的 int 类型
+
+## 类型转换
+
+- 阴性类型转换
+- 显性类型转换
+
+***隐性：***
+
+```python
+a = 1.0
+b = 1
+
+c = a+b #  2.0 b 阴性转化为1.0
+```
+***显性：***
+
+|函数|描述|
+|:---:|:---:|
+|int(x [,base])|将 x 转化为 int 类型|
+|float(x)|将 x 转化为 float 类型|
+|complex(real [,imag])|将 real 和 imag 转化为 complex 类型|
+|str(x)|将 x 转化为 str 类型|
+|bytes(x [,encoding[,errors]])|将 x 转化为 bytes 类型|
+|list(x)|将 x 转化为 list 类型|
+|tuple(x)|将 x 转化为 tuple 类型|
+|set(x)|将 x 转化为 set 类型|
+|dict(x)|将 x 转化为 dict 类型|
+|chr(x)|将 整数 x 转化为字符类型|
+|ord(x)|将 x 转化为编码类型|
+|bin(x)|将 x 转化为二进制类型|
+|oct(x)|将 x 转化为八进制类型|
+|hex(x)|将 x 转化为十六进制类型|
+|eval(x)|将 x 转化为表达式类型|
+|repr(x)|将 x 转化为字符串类型|
+|type(x)|将 x 转化为类型类型|
--- learn-python-by-examples.py
+++ learn-python-by-examples.py
@@ -2,7 +2,7 @@
 Author: shgopher shgopher@gmail.com
 Date: 2024-08-18 11:42:15
 LastEditors: shgopher shgopher@gmail.com
-LastEditTime: 2024-08-19 17:15:23
+LastEditTime: 2024-08-19 19:07:43
 FilePath: /PythonFamily/learn-python-by-examples.py
 Description: 
 
@@ -15,10 +15,111 @@ Copyright (c) 2024 by shgopher, All Rights Reserved.
 # 开始
 
 # 1. hello world
-print("hello world")
+print("hello world \n")
 
-# 2. 
-b = "hello world"
-c = 'h' 
+# 2. 数据类型
+a = 1 # 整数
+b = "hello world" # 字符串
+c = 'h' # 字符
+d = True # 布尔类型
+e = ['1',2.0,3] # 列表 :更灵活的切片
+f= (1,'2',1) # 元组
+g = {'1',1,1.0} # set ：只有key的map
+h = {'name':'shgopher','age':18} # map
 
-print(a, b, c)
+print(a,b,c,d,e,f,g,h,'\n')
+
+# 类型转换
+## 隐性转化
+j = 1
+i = 1.0
+value = j + i # 2.0 ,j 阴性转化为1.0 了
+print(value)
+## 显性转化
+### int 转换
+num_str = "8"
+num_int = int(num_str)
+print("将字符串 '10' 转换为整数：", num_int)
+
+### float 转换
+num = 5
+num_float = float(num)
+print("将整数 5 转换为浮点数：", num_float)
+
+### complex 转换
+real = 2
+imag = 3
+num_complex = complex(real, imag)
+print("将 2 和 3 转换为复数：", num_complex)
+
+### str 转换
+num = 100
+str_num = str(num)
+print("将整数 100 转换为字符串：", str_num)
+
+### bytes 转换
+text = "Hello"
+bytes_text = bytes(text, encoding='utf-8')
+print("将字符串 'Hello' 转换为字节：", bytes_text)
+
+### list 转换
+tuple_data = (1, 2, 3)
+list_data = list(tuple_data)
+print("将元组 (1, 2, 3) 转换为列表：", list_data)
+
+### tuple 转换
+list_data = [4, 5, 6]
+tuple_data = tuple(list_data)
+print("将列表 [4, 5, 6] 转换为元组：", tuple_data)
+
+### set 转换
+list_data = [7, 7, 8, 8, 9]
+set_data = set(list_data)
+print("将列表 [7, 7, 8, 8, 9] 转换为集合：", set_data)
+
+### dict 转换
+### 此处示例将两个列表组合成字典
+keys = ['a', 'b', 'c']
+values = [1, 2, 3]
+dict_data = dict(zip(keys, values))
+print("将两个列表组合成字典：", dict_data)
+
+### chr 转换
+num = 65
+char = chr(num)
+print("将 65 转换为字符：", char)
+
+### ord 转换
+char = 'A'
+num = ord(char)
+print("将字符 'A' 转换为编码：", num)
+
+### bin 转换
+num = 10
+bin_num = bin(num)
+print("将整数 10 转换为二进制：", bin_num)
+
+### oct 转换
+num = 10
+oct_num = oct(num)
+print("将整数 10 转换为八进制：", oct_num)
+
+### hex 转换
+num = 10
+hex_num = hex(num)
+print("将整数 10 转换为十六进制：", hex_num)
+
+### eval 转换
+expr = "2 + 3"
+result = eval(expr)
+print("对表达式 '2 + 3' 求值：", result)
+
+### repr 转换
+obj = [1, 2, 3]
+repr_str = repr(obj)
+print("将列表 [1, 2, 3] 转换为字符串表示：", repr_str)
+
+### type 转换
+num = 5
+print("5 的数据类型：", type(num))
+
